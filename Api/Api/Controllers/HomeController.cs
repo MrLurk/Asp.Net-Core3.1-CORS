c299a467e67f431bab2edd9ca508555a36241c62
CoCoWin <m15675306819@163.com>
2020-08-04 22:18:05 +0800

    初始化提交,完成 Asp.net core 3.1 跨域配置

---
+++ Api/Api/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var temp = new { ID = 1, Name = "张三", Age = 18 };
+            return Ok(temp);
+        }
+    }
+}
